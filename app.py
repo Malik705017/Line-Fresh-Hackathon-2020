e1e4871085cce9ad1d93a5720cfa4bbdf707900a
Malik <fhh1482@gmail.com>
2020-08-07 18:52:52 +0800

    DDD

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def handle_message(event):
         msg_to_user = "請輸入您的問題"
     elif(msg_from_user == "我要貼圖"):
         randNum1 = random.randint(1, 2)
-        randNum2 = random.randint(1, 20)
+        randNum2 = random.randint(1, 10)
         message = StickerSendMessage(
         package_id=str(randNum1),
         sticker_id=str(randNum2)
@@ -51,7 +51,7 @@ def handle_message(event):
     if(msg_from_user != "我要貼圖"):
         message = TextSendMessage(text=msg_to_user)
         line_bot_api.reply_message(event.reply_token, message)
-    print("finish")
+    
 
 
 import os
